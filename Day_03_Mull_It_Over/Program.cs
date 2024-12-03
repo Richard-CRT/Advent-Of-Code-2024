e763db56432618923e4f22f7820b80d4a724d84b
Richard-CRT <richard-crt@users.noreply.github.com>
2024-12-03 17:46:08 +0000

    Day 3 complete

---
+++ Day_03_Mull_It_Over/Program.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCodeUtilities;
+using System.Text.RegularExpressions;
+
+string input = AoC.GetInput();
+
+void P1()
+{
+    Int64 result = 0;
+    var matches = AoC.RegexMatch(input, "mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    int a;
+    int b;
+    foreach (Match match in matches)
+    {
+        a = int.Parse(match.Groups[1].Value);
+        b = int.Parse(match.Groups[2].Value);
+        result += (Int64)a * (Int64)b;
+    }
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    Int64 result = 0;
+    var matches = AoC.RegexMatch(input, "mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
+    int a;
+    int b;
+    bool enabled = true;
+    foreach (Match match in matches)
+    {
+        if (match.Groups[0].Value == "do()") enabled = true;
+        else if (match.Groups[0].Value == "don't()") enabled = false;
+        else if (enabled)
+        {
+            a = int.Parse(match.Groups[1].Value);
+            b = int.Parse(match.Groups[2].Value);
+            result += (Int64)a * (Int64)b;
+        }
+    }
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+P1();
+P2();

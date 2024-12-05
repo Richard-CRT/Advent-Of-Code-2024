7a4c79d10e0739e36fb7f7858b43d606d742d715
Richard-CRT <richard-crt@users.noreply.github.com>
2024-12-05 22:14:46 +0000

    Update AoCUtilities

--- AoCUtilities/AoCUtilities.cs
+++ AoCUtilities/AoCUtilities.cs
@@ -1,24 +1,50 @@
 ﻿//#define OVERRIDE
+
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCodeUtilities
 {
-    public class AoC
+    // Useful characters for debug and visualisation
+    // ┘ ┐ └ ┌ │ ─ 
+    // ┤ ┴ ├ ┬ ┼
+    //
+    // ╝ ╗ ╚ ╔ ║ ═
+    // ╣ ╩ ╠ ╦ ╬
+    //
+    // ╛ ╕ ╘ ╒
+    // ╡ ╧ ╞ ╤ ╪
+    //
+    // ╜ ╖ ╙ ╓
+    // ╢ ╨ ╟ ╥ ╫
+    //
+    // █▓▒░
+    // ▄▌▀▐
+    // ♥♦♣♠
+
+    public static class AoC
     {
-        public static int GCF(int a, int b)
+        public static Int64 GCF(Int64 a, Int64 b)
         {
             while (b != 0)
             {
-                int temp = b;
+                Int64 temp = b;
                 b = a % b;
                 a = temp;
             }
             return a;
         }
 
-        public static int LCM(int a, int b)
+        public static Int64 LCM(Int64 a, Int64 b)
         {
             return (a / GCF(a, b)) * b;
+        }
+
+        public static Int64 LCM(params Int64[] nums)
+        {
+            return nums.Aggregate(1, (Int64 lcm, Int64 num) => LCM(lcm, num));
         }
 
         static public void DebugClear()
@@ -93,6 +119,66 @@ namespace AdventOfCodeUtilities
 
                 return Convert.ToHexString(hashBytes);
             }
+        }
+
+        static public double RunWithStopwatch(Action action, Int64 repeats)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            for (Int64 i = 0; i < repeats; i++)
+                action();
+            sw.Stop();
+            double elapsedSeconds = (sw.ElapsedTicks / (double)Stopwatch.Frequency);
+            double secondsPerRun = elapsedSeconds / repeats;
+            return secondsPerRun;
+        }
+
+        /*
+        // Example
+        list_of_something.OrderByLambda(something => something.interesting_field, (x, y) => {
+	        if (x > y)
+		        return 1;
+	        else if (x < y)
+		        return -1;
+	        else
+		        return 0;
+        });
+        */
+        static public IOrderedEnumerable<TSource> OrderByLambda<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TKey?, TKey?, int> compareFunc)
+        => source.OrderBy(keySelector, new AoCComparer<TKey>(compareFunc, false));
+
+        static public IOrderedEnumerable<TSource> OrderByLambdaDescending<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TKey?, TKey?, int> compareFunc)
+        => source.OrderBy(keySelector, new AoCComparer<TKey>(compareFunc, true));
+
+        public static void ForEach<T>(
+            this IEnumerable<T> source,
+            Action<T> action)
+        {
+            foreach (T element in source)
+                action(element);
+        }
+    }
+
+    internal class AoCComparer<TKey> : IComparer<TKey>
+    {
+        private readonly Func<TKey?, TKey?, int> _compareFunc;
+        private readonly bool _invert;
+
+        public AoCComparer(Func<TKey?, TKey?, int> compareFunc, bool invert)
+        {
+            _compareFunc = compareFunc;
+            _invert = invert;
+        }
+
+        public int Compare(TKey? x, TKey? y)
+        {
+            int result = _compareFunc(x, y);
+            return _invert ? result * -1 : result;
         }
     }
 }

f433c7f3a317ef64b4595b624b34f8925d227d82
Richard-CRT <richard-crt@users.noreply.github.com>
2025-01-08 18:36:22 +0000

    Day 21 complete

--- Day_21_Keypad_Conundrum/Program.cs
+++ Day_21_Keypad_Conundrum/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AdventOfCodeUtilities;
+using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
 using System.Text.RegularExpressions;
+using System.Threading.Channels;
 
 List<string> codes = AoC.GetInputLines();
 
@@ -27,13 +29,15 @@ Dictionary<char, (int x, int y)> directionalKeyPadInputToCoord = new()
     { '<', (0,1) },
 };
 
-Dictionary<(char, char), List<string>> keyPadInputsToInstructions = new();
+// I don't really understand why it's better to do left, then vertical, then right (unless not allowed by the gap)
+
+Dictionary<(char, char), string> numericalKeyPadInputsToInstructions = new();
 foreach ((char c1, var coord1) in keyPadInputToCoord)
 {
     foreach ((char c2, var coord2) in keyPadInputToCoord)
     {
+        /*
         List<string> instructions = new();
-
         string horizontalDifference = "";
         if (coord2.x > coord1.x)
             horizontalDifference = new string('>', coord2.x - coord1.x);
@@ -48,62 +52,94 @@ foreach ((char c1, var coord1) in keyPadInputToCoord)
         // Special cases to avoid the gap
         if (coord1.x == 0 && coord1.y <= 2 && coord2.x >= 1 && coord2.y == 3)
         {
-            instructions.Add(horizontalDifference + verticalDifference);
+            instructions.Add(horizontalDifference + verticalDifference + 'A');
         }
         else if (coord1.x >= 1 && coord1.y == 3 && coord2.x == 0 && coord2.y <= 2)
         {
-            instructions.Add(verticalDifference + horizontalDifference);
+            instructions.Add(verticalDifference + horizontalDifference + 'A');
         }
         else
         {
-            instructions.Add(horizontalDifference + verticalDifference);
+            instructions.Add(verticalDifference + horizontalDifference + 'A');
             if (horizontalDifference != "" && verticalDifference != "")
-                instructions.Add(verticalDifference + horizontalDifference);
+                instructions.Add(horizontalDifference + verticalDifference + 'A');
         }
-        keyPadInputsToInstructions[(c1, c2)] = instructions;
+        numericalKeyPadInputsToInstructions[(c1, c2)] = instructions;
+        */
+        /*
+        if (coord2.x < coord1.x)
+        {
+            if (coord2.y > coord1.y)
+                instructions.Add(new string('<', coord1.x - coord2.x) + new string('v', coord2.y - coord1.y) + 'A');
+            else if (coord2.y < coord1.y)
+                instructions.Add(new string('<', coord1.x - coord2.x) + new string('^', coord1.y - coord2.y) + 'A');
+        }
+        else if (coord2.x > coord1.x)
+        {
+            if (coord2.y > coord1.y)
+                instructions.Add(new string('v', coord2.y - coord1.y) + new string('>', coord2.x - coord1.x) + 'A');
+            else if (coord2.y < coord1.y)
+                instructions.Add(new string('^', coord1.y - coord2.y) + new string('>', coord2.x - coord1.x) + 'A');
+        }
+        */
+        string leftInstructions = new string('<', Math.Max(0, coord1.x - coord2.x));
+        string rightInstructions = new string('>', Math.Max(0, coord2.x - coord1.x));
+        string upInstructions = new string('^', Math.Max(0, coord1.y - coord2.y));
+        string downInstructions = new string('v', Math.Max(0, coord2.y - coord1.y));
+        string instructions = leftInstructions + downInstructions + upInstructions + rightInstructions;
+        if ((coord2.x == 0 && coord1.y == 3) || (coord1.x == 0 && coord2.y == 3))
+        {
+            char[] chars = instructions.ToCharArray();
+            Array.Reverse(chars);
+            instructions = new string(chars);
+        }
+        instructions = instructions + 'A';
+        numericalKeyPadInputsToInstructions[(c1, c2)] = instructions;
     }
 }
 
+/*
 Dictionary<(char, char), List<string>> directionalKeyPadInputsToInstructions = new()
 {
 
-    { ('<', '<'), new List<string>() { "" } },
-    { ('<', 'v'), new List<string>() { ">" } },
-    { ('<', '>'), new List<string>() { ">>" } },
-    { ('<', '^'), new List<string>() { ">^" } },
-    { ('<', 'A'), new List<string>() { ">>^" } }, // Don't bother with sequences that involve changing direction in middle, they won't be ideal
+    { ('<', '<'), new List<string>() { "A" } },
+    { ('<', 'v'), new List<string>() { ">A" } },
+    { ('<', '>'), new List<string>() { ">>A" } },
+    { ('<', '^'), new List<string>() { ">^A" } },
+    { ('<', 'A'), new List<string>() { ">>^A" } }, // Don't bother with sequences that involve changing direction in middle, they won't be ideal
 
-    { ('v', 'v'), new List<string>() { "" } },
-    { ('v', '<'), new List<string>() { "<" } },
-    { ('v', '>'), new List<string>() { ">" } },
-    { ('v', '^'), new List<string>() { "^" } },
-    { ('v', 'A'), new List<string>() { "^>", ">^" } },
+    { ('v', 'v'), new List<string>() { "A" } },
+    { ('v', '<'), new List<string>() { "<A" } },
+    { ('v', '>'), new List<string>() { ">A" } },
+    { ('v', '^'), new List<string>() { "^A" } },
+    { ('v', 'A'), new List<string>() { "^>A", ">^A" } },
 
-    { ('>', '>'), new List<string>() { "" } },
-    { ('>', '<'), new List<string>() { "<<" } },
-    { ('>', 'v'), new List<string>() { "<" } },
-    { ('>', '^'), new List<string>() { "<^", "^<" } },
-    { ('>', 'A'), new List<string>() { "^" } },
+    { ('>', '>'), new List<string>() { "A" } },
+    { ('>', '<'), new List<string>() { "<<A" } },
+    { ('>', 'v'), new List<string>() { "<A" } },
+    { ('>', '^'), new List<string>() { "<^A", "^<A" } },
+    { ('>', 'A'), new List<string>() { "^A" } },
 
-    { ('^', '^'), new List<string>() { "" } },
-    { ('^', '<'), new List<string>() { "v<" } },
-    { ('^', 'v'), new List<string>() { "v" } },
-    { ('^', '>'), new List<string>() { "v>", ">v" } },
-    { ('^', 'A'), new List<string>() { ">" } },
+    { ('^', '^'), new List<string>() { "A" } },
+    { ('^', '<'), new List<string>() { "v<A" } },
+    { ('^', 'v'), new List<string>() { "vA" } },
+    { ('^', '>'), new List<string>() { "v>A", ">vA" } },
+    { ('^', 'A'), new List<string>() { ">A" } },
 
-    { ('A', 'A'), new List<string>() { "" } },
-    { ('A', '<'), new List<string>() { "v<<" } }, // Don't bother with sequences that involve changing direction in middle, they won't be ideal
-    { ('A', 'v'), new List<string>() { "v<", "<v" } },
-    { ('A', '>'), new List<string>() { "v" } },
-    { ('A', '^'), new List<string>() { "<" } },
+    { ('A', 'A'), new List<string>() { "A" } },
+    { ('A', '<'), new List<string>() { "v<<A" } }, // Don't bother with sequences that involve changing direction in middle, they won't be ideal
+    { ('A', 'v'), new List<string>() { "v<A", "<vA" } },
+    { ('A', '>'), new List<string>() { "vA" } },
+    { ('A', '^'), new List<string>() { "<A" } },
 };
+*/
 
-/*
 Dictionary<(char, char), string> directionalKeyPadInputsToInstructions = new();
 foreach ((char c1, var coord1) in directionalKeyPadInputToCoord)
 {
     foreach ((char c2, var coord2) in directionalKeyPadInputToCoord)
     {
+        /*
         string instructions = "";
         string horizontalDifference = "";
         if (coord2.x > coord1.x)
@@ -129,11 +165,150 @@ foreach ((char c1, var coord1) in directionalKeyPadInputToCoord)
         {
             instructions = horizontalDifference + verticalDifference;
         }
+        */
+
+        string leftInstructions = new string('<', Math.Max(0, coord1.x - coord2.x));
+        string rightInstructions = new string('>', Math.Max(0, coord2.x - coord1.x));
+        string upInstructions = new string('^', Math.Max(0, coord1.y - coord2.y));
+        string downInstructions = new string('v', Math.Max(0, coord2.y - coord1.y));
+        string instructions = leftInstructions + downInstructions + upInstructions + rightInstructions;
+        if ((coord2.x == 0 && coord1.y == 0) || (coord1.x == 0 && coord2.y == 0))
+        {
+            char[] chars = instructions.ToCharArray();
+            Array.Reverse(chars);
+            instructions = new string(chars);
+        }
+        instructions = instructions + 'A';
         directionalKeyPadInputsToInstructions[(c1, c2)] = instructions;
     }
 }
-*/
 
+List<string> recurse(List<string> inputs, char[] robotLocs, int depth = 0)
+{
+    if (depth == robotLocs.Length)
+    {
+        return inputs;
+    }
+    else
+    {
+        Dictionary<(char, char), string> inputsToInstructionsDictionaryToUse;
+        if (depth == 0)
+            inputsToInstructionsDictionaryToUse = numericalKeyPadInputsToInstructions;
+        else
+            inputsToInstructionsDictionaryToUse = directionalKeyPadInputsToInstructions;
+
+        //Int64 minLengthOfNewInputs = Int64.MaxValue;
+        //Int64 minChangesOfNewInputs = Int64.MaxValue;
+        List<string> newInputs = new();
+        foreach (string input in inputs)
+        {
+            List<string> newInputsFromThisInput = new() { "" };
+            foreach (char charInCode in input)
+            {
+                List<string> possibleInstructions = new() { inputsToInstructionsDictionaryToUse[(robotLocs[depth], charInCode)] };
+                if (possibleInstructions.Count == 1)
+                {
+                    string instructions = possibleInstructions[0];
+                    for (int i = 0; i < newInputsFromThisInput.Count; i++)
+                        newInputsFromThisInput[i] += instructions;
+                }
+                else
+                {
+                    int initialCount = newInputsFromThisInput.Count;
+                    for (int i = 0; i < initialCount; i++)
+                    {
+                        string originalNewInput = newInputsFromThisInput[i];
+                        newInputsFromThisInput[i] = originalNewInput + possibleInstructions[0];
+                        // Add new ones for the others
+                        for (int j = 1; j < possibleInstructions.Count; j++)
+                        {
+                            newInputsFromThisInput.Add(originalNewInput + possibleInstructions[j]);
+                        }
+                    }
+                }
+                robotLocs[depth] = charInCode;
+            }
+
+            /*
+            Int64 minChangesOfNewInputsFromThisInput = Int64.MaxValue;
+            foreach (string newInputFromThisInput in newInputsFromThisInput)
+            {
+                Int64 changes = 0;
+                for (int i = 0; i < newInputFromThisInput.Length - 1; i++)
+                {
+                    if (newInputFromThisInput[i] != newInputFromThisInput[i + 1])
+                        changes++;
+                }
+                if (changes < minChangesOfNewInputsFromThisInput) minChangesOfNewInputsFromThisInput = changes;
+            }
+            if (minChangesOfNewInputsFromThisInput < minChangesOfNewInputs)
+            {
+                minChangesOfNewInputs = minChangesOfNewInputsFromThisInput;
+                newInputs = newInputsFromThisInput;
+            }
+            else if (minChangesOfNewInputsFromThisInput == minChangesOfNewInputs)
+            {
+                newInputs.AddRange(newInputsFromThisInput);
+            }
+            else
+            {
+
+            }
+            */
+
+
+            /*
+            int lengthOfNewInputsFromThisInput = newInputsFromThisInput[0].Length;
+            if (lengthOfNewInputsFromThisInput < minLengthOfNewInputs)
+            {
+                minLengthOfNewInputs = lengthOfNewInputsFromThisInput;
+                newInputs = newInputsFromThisInput;
+            }
+            else if (lengthOfNewInputsFromThisInput == minLengthOfNewInputs)
+            {
+                newInputs.AddRange(newInputsFromThisInput);
+            }
+            else
+            {
+
+            }
+            */
+
+            newInputs.AddRange(newInputsFromThisInput);
+        }
+        return recurse(newInputs, robotLocs, depth + 1);
+    }
+}
+
+Dictionary<(string, int), Int64> cache = new();
+Int64 recurse2(string sequence, int robotsCount, int depth = 0)
+{
+    if (depth == robotsCount)
+        return sequence.Length;
+    else if (cache.TryGetValue((sequence, depth), out Int64 length))
+        return length;
+    else
+    {
+        Dictionary<(char, char), string> inputsToInstructionsDictionaryToUse;
+        if (depth == 0)
+            inputsToInstructionsDictionaryToUse = numericalKeyPadInputsToInstructions;
+        else
+            inputsToInstructionsDictionaryToUse = directionalKeyPadInputsToInstructions;
+
+        Int64 totalLength = 0;
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            (char, char) pair;
+            if (i == 0)
+                pair = ('A', sequence[i]);
+            else
+                pair = (sequence[i - 1], sequence[i]);
+            totalLength += recurse2(inputsToInstructionsDictionaryToUse[pair], robotsCount, depth + 1);
+        }
+        cache[(sequence, depth)] = totalLength;
+        return totalLength;
+    }
+}
 
 void P1()
 {
@@ -145,49 +320,12 @@ void P1()
 
     foreach (string code in codes)
     {
-        List<string> keyPadInstructionsInputs = new() { code };
-        for (int depth = 0; depth < robotLocs.Length; depth++)
-        {
-            Dictionary<(char, char), List<string>> dictionaryToUse;
-            if (depth == 0)
-                dictionaryToUse = keyPadInputsToInstructions;
-            else
-                dictionaryToUse = directionalKeyPadInputsToInstructions;
-
-            List<string> keyPadInstructions = new() { };
-            foreach (string keyPadInstructionsInput in keyPadInstructionsInputs)
-            {
-                List<string> keyPadInstructionsForThisInput = new() { "" };
-                foreach (char charInCode in keyPadInstructionsInput)
-                {
-                    List<string> possibleInstructions = dictionaryToUse[(robotLocs[depth], charInCode)];
-                    if (possibleInstructions.Count == 1)
-                    {
-                        for (int i = 0; i < keyPadInstructionsForThisInput.Count; i++)
-                            keyPadInstructionsForThisInput[i] += possibleInstructions[0] + 'A';
-                    }
-                    else
-                    {
-                        var oldDirectionalKeyPadInstructionsForThisInput = keyPadInstructionsForThisInput;
-                        keyPadInstructionsForThisInput = new();
-                        foreach (string s in oldDirectionalKeyPadInstructionsForThisInput)
-                        {
-                            foreach (var possibleInstruction in possibleInstructions)
-                                keyPadInstructionsForThisInput.Add(s + possibleInstruction + 'A');
-                        }
-                    }
-                    robotLocs[depth] = charInCode;
-                }
-                keyPadInstructions.AddRange(keyPadInstructionsForThisInput);
-            }
-
-            keyPadInstructionsInputs = keyPadInstructions;
-        }
+        List<string> inputs = recurse(new() { code }, robotLocs);
 
         int parse = int.Parse(code.Trim('A'));
-        int length = keyPadInstructionsInputs.MinBy(s => s.Length)!.Length;
+        int length = inputs.MinBy(s => s.Length)!.Length;
         result += parse * length;
-        Console.WriteLine($"{length}, {parse}");
+        //Console.WriteLine($"{length}, {parse}");
     }
 
     Console.WriteLine(result);
@@ -196,7 +334,21 @@ void P1()
 
 void P2()
 {
-    int result = 0;
+    Int64 result = 0;
+
+    char[] robotLocs = new char[3];
+    for (int i = 0; i < robotLocs.Length; i++)
+        robotLocs[i] = 'A';
+
+    foreach (string code in codes)
+    {
+        Int64 length = recurse2(code, 26);
+
+        int parse = int.Parse(code.Trim('A'));
+        result += parse * length;
+        //Console.WriteLine($"{length}, {parse}");
+    }
+
     Console.WriteLine(result);
     Console.ReadLine();
 }
